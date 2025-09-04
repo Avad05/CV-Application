95ac75ce8592efbbbc756ddb9022db52062b6329
Avadhut <avadhutkaskar@gmail.com>
2025-09-04 15:31:24 +0530

    Toggle added to Education

--- CV-Application/src/components/Experience.jsx
+++ CV-Application/src/components/Experience.jsx
@@ -1,11 +1,53 @@
-import { FaBox } from "react-icons/fa"
+import { useState } from "react"
+import { FaBriefcase } from "react-icons/fa"
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function ExperienceDetails(){
 
-    
+        const [company, setCompany] = useState("")
+        const [job, setJob] = useState("")
+        const [startYear, setStartYear] = useState("")
+        const [endYear, setEndYear] = useState("")
+        const [location, setLocation] = useState("")
            return(
             <>
-              <h2><FaBox />Experience</h2>
+              <h2><FaBriefcase /> Experience</h2>
+              <label className="required">Company Name</label> <br />
+              <input type="text"
+              value={company}
+              onChange={(event) => setCompany(event.target.value)}
+              /><br />
+
+              <label className="required">Position Title</label> <br />
+              <input type="text"
+              value={job}
+              onChange={(event) => setJob(event.target.value)}
+              />< br />
+
+              <label className='required'>StartYear</label> <br /> 
+                 <DatePicker 
+                  selected={startYear}
+                  onChange={(date) => setStartYear(date)}
+                  dateFormat="yyyy" // Format to display only the year
+                  showYearPicker // Enable year-only selection
+                  /> <br />
+
+                <label className='required'>EndYear</label> <br />
+                    <DatePicker 
+                      selected={endYear}
+                      onChange={(date) => setEndYear(date)}
+                      dateFormat="yyyy" // Format to display only the year
+                      showYearPicker // Enable year-only selection
+                     /> <br /> 
+
+<                label>Location</label> <br />
+                 <input type='text'
+                 value={location}
+                 onChange={(event) => setLocation(event.target.value)}
+                  />
+                            
+
             </>
            )
 }
--- CV-Application/src/components/Qualification.jsx
+++ CV-Application/src/components/Qualification.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import { FaSchool } from 'react-icons/fa'
+import { FaGraduationCap } from 'react-icons/fa'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -12,52 +12,70 @@ export default function QualificationDetails(){
     const [selectedYear, setSelectedYear]  = useState("");
     const [end, setEnd] = useState("");
     const [location, setLocation] = useState("");
-        return(
-            <>
-            <h2><FaSchool /> Education</h2>
-
-            <label className='required'>School</label> <br />
-            <input type='text'
-            value={school}
-            onChange={(event) => setSchool(event.target.value)}
-             /> <br />
-
-             <label className='required'>Degree</label> <br />
-             <input type='text'
-             value={degree}
-             onChange={(event) => setDegree(event.target.value)}
-             />
-            <br />
-
-             <label className='required'>StartYear</label> <br /> 
-              <DatePicker 
-             selected={selectedYear}
-             onChange={(date) => setSelectedYear(date)}
-             dateFormat="yyyy" // Format to display only the year
-             showYearPicker // Enable year-only selection
-             /> <br />
-              
-
-             <label className='required'>EndYear</label> <br />
-             <DatePicker 
-             selected={end}
-             onChange={(date) => setEnd(date)}
-             dateFormat="yyyy" // Format to display only the year
-             showYearPicker // Enable year-only selection
-             /> <br />  
-
-             <label>Location</label> <br />
-             <input type='text'
-             value={location}
-             onChange={(event) => setLocation(event.target.value)}
-
-             />
-             <br />
+    const [showResult, setShowResult] = useState(false);
+    const onClick = () => {setShowResult(!showResult);}
 
 
-             
+   
 
-            </>
+
+    return(
+        <>
+           
+          <h2 onClick={onClick}><FaGraduationCap /> Education</h2>
+          {showResult ? null : null}
+
+
+          {showResult &&( 
+        <>
+        <label className='required'>School</label> <br />
+     <input type='text'
+     value={school}
+     onChange={(event) => setSchool(event.target.value)}
+      /> <br />
+
+
+      <label className='required'>Degree</label> <br />
+     <input type='text'
+     value={degree}
+      onChange={(event) => setDegree(event.target.value)}
+     />
+      <br />
+
+     <label className='required'>StartYear</label> <br /> 
+       <DatePicker 
+    selected={selectedYear}
+    onChange={(date) => setSelectedYear(date)}
+      dateFormat="yyyy" // Format to display only the year
+      showYearPicker // Enable year-only selection
+      /> <br />
+
+
+   <label className='required'>EndYear</label> <br />
+   <DatePicker 
+  selected={end}
+   onChange={(date) => setEnd(date)}
+   dateFormat="yyyy" // Format to display only the year
+     showYearPicker // Enable year-only selection
+         /> <br />  
+
+     <label>Location</label> <br />
+      <input type='text'
+      value={location}
+    onChange={(event) => setLocation(event.target.value)}
+
+     />
+      <br />
+      </>          
+          )}
+           
+         </>
         )
+        
+           
 
+
+                      
+
+         
 }
